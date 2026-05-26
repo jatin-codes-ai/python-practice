8a89082d67b23c0bc0c2396a17600f7b2a3bf6ca
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-05-26 11:55:48 +0530

    Day 4 : Basic temperature converter & fixed previous files

--- My Learning/Week 1/Day 4/day4_even-odd_checker.py
+++ My Learning/Week 1/Day 4/day4_even-odd_checker.py
@@ -9,47 +9,65 @@ print("="*20 + 'Even/Odd Checker' + "="*20)
 def is_even(number) :
     return number % 2 == 0 
        
+while True :
+    # list of numbers
+    count = int(input("How many numbers to check?: "))
+    num_list = []
+    even = []
+    odd = []
 
-# list of numbers
-count = int(input("How many numbers to check?: "))
-num_list = []
-even = []
-odd = []
-
-# looping on list 
-i = 1
-if count == 0 :
-    print("No number given.")
+    # looping on list 
+    if count == 0 :
+        print("No number given.")
     
-while i <= count :
+    for i in range(1, count + 1) :
 
-    print()
-    ip = int(input(f"Enter number {i}: "))
-    num_list.append(ip)
+        print()
+        ip = int(input(f"Enter number {i}: "))
+        num_list.append(ip)
 
-    # if count :
+        # if count :
     
-    # function call
-    if is_even(ip) :
+        # function call
+        if is_even(ip) :
                 print(f"{ip} is even.")
                 even.append(ip)
-    else: 
+        else: 
                 print(f"{ip} is odd.")
-                odd.append(ip)
-    i += 1
+                odd.append(ip) 
     
-print(f"\nTotal: {count} numbers")
-if len(even) :
-    print(f"Even numbers: {even}")
-    print(f"Biggest even number: {max(even)}")
-    print(f"Smallest even number: {min(even)}")
-    print(f"Even: {len(even)}")
+    print(f"\nNumbers entered: {num_list}")
+    print(f"Total: {count} numbers")
+    if len(even) :
+        print(f"\nEven numbers: {even}")
+        print(f"Biggest even number: {max(even)}")
+        print(f"Smallest even number: {min(even)}")
+        print(f"Even: {len(even)}")
 
-if len(odd) :
-    print(f"\nOdd numbers: {odd}")
-    print(f"Biggest odd number: {max(odd)}")
-    print(f"Smallest odd number: {min(odd)}")
-    print(f"Odd: {len(odd)}")
+    if len(odd) :
+        print(f"\nOdd numbers: {odd}")
+        print(f"Biggest odd number: {max(odd)}")
+        print(f"Smallest odd number: {min(odd)}")
+        print(f"Odd: {len(odd)}")
+
+    while True :
+        again = input("want to check more numbers(Y/N): ").upper()
+
+        if not (again == 'Y' or again == 'N') :
+            print("Try again.")
+
+        else :
+             break
+        
+    if again == 'N' :
+         print("Thank you!")
+         break
+    print("Continuing...")
+    
+
+    
+    
+    
 
 
 # Ending        
