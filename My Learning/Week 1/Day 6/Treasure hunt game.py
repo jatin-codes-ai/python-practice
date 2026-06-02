1b5151ed5c7fa7193597188bb9d4f2c375abef5a
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-03 01:39:47 +0530

    Day 6: Tresure hunt game

---
+++ My Learning/Week 1/Day 6/Treasure hunt game.py
@@ -0,0 +1,29 @@
+# Treasure hunt game
+print()
+treasure = [(2,3),(5,7),(4,8),(1,9),(6,5)]
+print("TREASURE HUNT - 5 treasures hidden!")
+print("Guess Coordinates (0-10 range)")
+found = []
+
+for attempt in range(1,6):
+    print(f"\nAttempt {attempt}/5")
+    x = int(input("Enter X: "))
+    y = int(input("Enter Y: "))
+    guess = (x,y)
+
+    if guess in found:
+        print("Already obtained treasure.")
+    else:
+        distance = abs(x - treasure[0][0]) + abs(y - treasure[0][1])
+        if guess in treasure :
+            found.append(guess)
+            print(f"Treasure found at {guess}! found: {len(found)}/5")
+
+        elif distance <= 2 : 
+            print("Close.")
+        else: 
+            print("Cold.")
+
+print(f"\nGame Over! Total score: {len(found)}/5. ")
+
+print()
