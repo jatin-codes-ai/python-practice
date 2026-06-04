663e9994743a5b64bdbf6c796e0b814358d8001f
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-05 00:15:12 +0530

    Day 7: Set basics

--- My Learning/Week 1/Day 7/Set intro.py
+++ My Learning/Week 1/Day 7/Set intro.py
@@ -13,4 +13,20 @@ print(f"After removing Javascript: {languages}")
 
 print(f"Is Python in set? {'Python' in languages}")
 
+
+visitors = ["jatin", "ravi", "jatin", "amit", "ravi", "jatin"]
+unique_visitors = set(visitors)
+
+print(f"\nTotal visits: {len(visitors)}")
+print(f"Unique visitors: {len(unique_visitors)}")
+print(f"Unique names: {unique_visitors}")
+
+
+my_skills = {"Python", "Capcut", "Calisthenics"}
+needed_for_job = {"Python", "SQL", "ML", "Pandas"}
+already_have = my_skills.intersection(needed_for_job)
+print(f"\nSkills I already have: {already_have}")
+need_to_learn = needed_for_job - my_skills
+print(f"Skills I still need: {need_to_learn}")
+
 print()
