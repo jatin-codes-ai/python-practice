564369f304b8f2211ac8a431bf092ff3f6e25a03
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-04 23:50:25 +0530

    Day 7: Set intro

---
+++ My Learning/Week 1/Day 7/Set intro.py
@@ -0,0 +1,16 @@
+# Set intro 
+
+print()
+
+languages  = {"Python", "Python", "SQL", "Javascript", "Python"}
+print(f"My languages: {languages}")
+
+languages.add("Java")
+print(f"After adding Java: {languages}")
+
+languages.remove("Javascript")
+print(f"After removing Javascript: {languages}")
+
+print(f"Is Python in set? {'Python' in languages}")
+
+print()
