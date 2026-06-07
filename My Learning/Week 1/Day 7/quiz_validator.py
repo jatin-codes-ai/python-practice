7e0c8dc99f33e4360d2a8aea4c85dd3857116959
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-07 23:47:31 +0530

    Day 7: Quiz validator in progress

---
+++ My Learning/Week 1/Day 7/quiz_validator.py
@@ -0,0 +1,14 @@
+# quiz_validator
+
+print()
+
+correct_answers = {'new delhi', 'pacific', 'peacock', 'hockey', 'tiger'}
+answers = []
+students_answers = set(answers)
+
+answer1 = input("Capital of India? ").lower()
+answers.append(answer1)
+answer2 = input("Largest ocean? ").lower()
+answers.append(answer2)
+answer3 = input("National bird of India? ").lower()
+answers.append(answer3)
