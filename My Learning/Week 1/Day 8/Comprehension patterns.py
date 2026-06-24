5cd3a8204f82ac369126ba39ebe37e0ebac3f8a4
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-24 23:55:14 +0530

    Day 8: Comprehension patterns

---
+++ My Learning/Week 1/Day 8/Comprehension patterns.py
@@ -0,0 +1,14 @@
+# Comprehension patterns
+print()
+
+# 1. list of students report
+students = [
+    ("Jatin",[89, 98, 87]),
+    ("Neeraj",[78,67,87]),
+    ("Bhavishya",[88, 78, 96])
+]
+
+averages = {name: round(sum(marks)/len(marks), 2) for name, marks in students}
+print(f"Results: {averages}")
+
+print()
