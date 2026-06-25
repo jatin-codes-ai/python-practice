d89ae33b0a1866473a44e3e33301c93a0d7f095a
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-06-25 23:59:36 +0530

    Day 8: Comprehensions mehtods

--- My Learning/Week 1/Day 8/Comprehension patterns.py
+++ My Learning/Week 1/Day 8/Comprehension patterns.py
@@ -7,8 +7,17 @@ students = [
     ("Neeraj",[78,67,87]),
     ("Bhavishya",[88, 78, 96])
 ]
-
 averages = {name: round(sum(marks)/len(marks), 2) for name, marks in students}
 print(f"Results: {averages}")
+
+# 2. Tuple unpacking in comprehension
+data = [('a', 1), ('b', 2), ('c', 3)]
+# result = []
+# for item in data : # old way 
+#     letter, number = item
+#     result.append(letter * number)
+
+result = [letter * number for letter, number in data ] # comprehension way
+print(f"Results: {result}")
 
 print()
