2db23da7fde1fa28a1dcf6b9975c2354b68442c6
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-07-19 00:37:52 +0530

    Day 11: Quote saver v2

--- My Learning/Week 1/Day 11/quote saver.py
+++ My Learning/Week 1/Day 11/quote saver.py
@@ -3,6 +3,7 @@
 print('''\n========================================
          QUOTE SAVER — v1
 ========================================''')
+
 while True:
     print('''\n1. Add a new quote
 2. View all quotes
@@ -12,13 +13,21 @@ while True:
     print(f"Your choice: {choice}")
     if choice == 1 :
         quote = input("Enter your quote: ")
+        with open("quotes.txt", "a") as file:
+            file.write(quote)
         print("Quote saved!")
     elif choice == 2 :
         print("All Quotes ---")
+        with open("quotes.txt", "r") as file:
+            for index, quote in enumerate(file, start=1) :
+                print(f"Quote {index}. {quote.strip()}")
         print("-" * 15 )
         print()
     elif choice == 3 : 
-        print("You have saved 2 quotes so far.")
+        with open("quotes.txt", "r") as file:
+            total_lines = len(file.readlines())
+        
+        print(f"You have saved {total_lines} quotes so far.")
     elif choice == 4 :
         print("Goodbye!")
         break
