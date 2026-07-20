7d0da2a3800dd4fac8365efc38ee5253a679b19f
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-07-20 23:57:33 +0530

    Day 11: Quote saver

--- My Learning/Week 1/Day 11/quote saver.py
+++ My Learning/Week 1/Day 11/quote saver.py
@@ -1,36 +1,40 @@
 # quote saver
 
-print('''\n========================================
-         QUOTE SAVER — v1
-========================================''')
+print("\n========================================")
+print("         QUOTE SAVER — v1")
+print("========================================")
 
 while True:
     print('''\n1. Add a new quote
 2. View all quotes
 3. Count quotes
 4. Exit''')
-    choice = int(input("\nEnter your choice: "))
+    
+    choice = input("\nEnter your choice: ")
     print(f"Your choice: {choice}")
-    if choice == 1 :
+
+    if choice == "1" :
         quote = input("Enter your quote: ")
         with open("quotes.txt", "a") as file:
-            file.write(quote)
+            file.write(quote + "\n")
         print("Quote saved!")
-    elif choice == 2 :
+
+    elif choice == "2" :
         print("All Quotes ---")
         with open("quotes.txt", "r") as file:
             for index, quote in enumerate(file, start=1) :
                 print(f"Quote {index}. {quote.strip()}")
-        print("-" * 15 )
-        print()
-    elif choice == 3 : 
+        print("-" * 15 + "\n")
+
+    elif choice == "3" : 
         with open("quotes.txt", "r") as file:
             total_lines = len(file.readlines())
-        
         print(f"You have saved {total_lines} quotes so far.")
-    elif choice == 4 :
+
+    elif choice == "4" :
         print("Goodbye!")
         break
+
     else :
         print("Invalid choice, please pick from 1-4.")
 
