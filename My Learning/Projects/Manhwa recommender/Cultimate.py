8a9fb514c89cdcac568e1a20de535791f114d0cb
jatin-codes-ai <jatin.kr11112002@gmail.com>
2026-08-02 00:17:48 +0530

    Project cultimate making

--- My Learning/Projects/Manhwa recommender/Cultimate.py
+++ My Learning/Projects/Manhwa recommender/Cultimate.py
@@ -6,13 +6,30 @@ print("             CULTIMATE")
 print("Your Manhwa Recommendation Engine")
 print("=" * 40)
 
-print()
-print("Menu:")
-print("1. View entire database")
-print("2. Add new manhwa")
-print("3. Get recommendations")
-print("4. Filter by mood")
-print("5. Exit")
+
+def get_input(prompt):
+
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Not a valid number, Try again.")
+
+while True:
+
+    print()
+    print("Menu:")
+    print("1. View entire database")
+    print("2. Add new manhwa")
+    print("3. Get recommendations")
+    print("4. Filter by mood")
+    print("5. Exit")
+
+    choice = input("Your choice: ")
+
+    if choice = '1':
+
+
 
 print("=" * 40)
 print("             END")
